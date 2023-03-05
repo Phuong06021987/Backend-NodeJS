32200182dc607f260e42a694e1184bf0d61ea986
unknown <phuong.tm0602@gmail.com>
2023-03-06 01:13:36 +0700

    use npm

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,16 @@
+import db from '../models/index';
 
-let getHomePage = (req, res) => {
+let getHomePage = async (req, res) => {
+    try{
+        let data = await db.User.findAll();
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+        });
+    }catch(e){
+        console.log(e)
+    }
     // return res.send('Hello world from controller')
-    return res.render('homepage.ejs');
+    
 }
 
 let getAboutPage = (req, res) => {
