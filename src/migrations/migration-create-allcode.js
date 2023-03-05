38b1820644af66a546e3c76ca6f2abd437d1c306
unknown <phuong.tm0602@gmail.com>
2023-03-06 02:28:07 +0700

    use npm

--- src/migrations/migration-create-allcode.js
+++ src/migrations/migration-create-allcode.js
@@ -1,27 +1,24 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('allcode', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      email: {
+      key: {
         type: Sequelize.STRING
       },
-      firstName: {
+      type: {
         type: Sequelize.STRING
       },
-      lastName: {
+      valueVi: {
         type: Sequelize.STRING
       },
-      address: {
+      valueEn: {
         type: Sequelize.STRING
-      },
-      gender: {
-        type: Sequelize.BOOLEAN
       },
 
       createdAt: {
@@ -35,6 +32,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('allcode');
   }
 };
